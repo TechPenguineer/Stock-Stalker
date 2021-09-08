7f1b9e8151719e23070bd8b8946b9f9eb4769165
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-08 17:59:04 -0400

    added main app

---
+++ app/viewer.py
@@ -0,0 +1,24 @@
+
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui
+import pyqtgraph.console
+import numpy as np
+
+from pyqtgraph.dockarea import *
+
+TITLE = "Stock Stalker - By TechPenguineer"
+window = pg.plot()
+window.setWindowTitle(TITLE)
+
+Y=[6,3,5,1,-2,-5,3]
+X=[1,2,3,4,5,6,7]
+
+window.showGrid(y=True)
+window.addLegend()
+GRAPH = window.plot(X, Y, fillLevel=2, pen ='g', width=2, symbolPen ='g', name ='Price')
+window.addItem(GRAPH)
+
+
+
+if __name__ == '__main__':
+    pg.exec()
